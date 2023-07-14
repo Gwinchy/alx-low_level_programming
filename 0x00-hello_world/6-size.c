0552cac8e9e4514a6bb959ce4e5612423332bc34
Gwinchy <gmunyui254@gmail.com>
2023-07-14 02:23:33 -0700

    Size of files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+# include <stdio.h>
 /**
  * main - Antry point
  * 
@@ -13,6 +13,5 @@ int main(void)
 	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	printf("Size of double: %zu byte(s)\n", sizeof(double));
-	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 	return 0;
 }
