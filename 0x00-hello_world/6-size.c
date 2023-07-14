a02c5b7cf72de01fa6a8461ba19450bf5de526a3
Gwinchy <gmunyui254@gmail.com>
2023-07-14 02:27:46 -0700

    6. Sizr is not grandeaur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	printf("Size of int: %zu byte(s)\n", sizeof(int));
-	printf("Size of short: %zu byte(s)\n", sizeof(short));
-	printf("Size of long: %zu byte(s)\n", sizeof(long));
-	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
-	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long: %ld byte(s)\n", sizeof(long));
+	printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
 	return 0;
 }
