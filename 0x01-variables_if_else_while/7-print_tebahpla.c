7469de61690f032f7f5e10c2a50b173a5d77e400
Gwinchy <gmunyui254@gmail.com>
2023-07-15 16:21:16 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/*
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+		putchar(k);
+	putchar(\n);
+	return (0);
+}
