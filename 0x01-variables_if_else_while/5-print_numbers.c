7b1b3e3f153457d2986850e42909227fefcb7341
Gwinchy <gmunyui254@gmail.com>
2023-07-15 15:15:57 -0700

    qw

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+ * main - Start point
+ *
+ * Return:  0 Success
+ */
+
+int main(void)
+
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+
+		printf("%d", p);
+
+		printf("\n");
+
+	return (0);
+
+}
