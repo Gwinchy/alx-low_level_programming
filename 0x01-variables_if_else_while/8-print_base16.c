4ceed857b671e8db4e59592cde6a6d514a599cbc
Gwinchy <gmunyui254@gmail.com>
2023-07-15 16:42:17 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r = '0'; r <= 9; r++)
+		char r;
+
+	for (s = 'a'; s >= f; s++)
+		putchar(r);
+	putchar(s);
+	putchar('\n');
+	return (0);
+}
