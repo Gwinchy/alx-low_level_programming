effb0a21594fc8e0e58ceedd5356f10de8ea61fd
Gwinchy <gmunyui254@gmail.com>
2023-07-15 13:58:07 -0700

    b

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
+
 /*
  * main - Starting point
  *
  * Return Always 0 (Success)
  */
+
 int main(void)
-	{
+
+
+{
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
+
 	{
+
 		if (i != 'e' && i != 'q')
+
 		{
+
 	putchar(i);
+
 	}
+
 	}
+
 	putchar('\n');
+
 	return (0);
+
 }
