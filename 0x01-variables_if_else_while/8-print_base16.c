aad60cb0ee3ea5960781658ea944071dd76caa14
Gwinchy <gmunyui254@gmail.com>
2023-07-15 17:19:17 -0700

    $$$

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@ int main(void)
 	int r;
 
 	for (r = '1'; r < '10'; r++)
+	{
 		putchar(r+ '0');
-	for (r = '1'; r < '8'; r++)
+	}
+	for (r = '1'; r < '6'; r++)
+	{
 		putchar(r+ 'a');
+	}
 	putchar('\n');
 	return (0);
 }
