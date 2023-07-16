8d2ee03b2163482b36ac75e867aae9bd954fed91
Gwinchy <gmunyui254@gmail.com>
2023-07-15 19:02:51 -0700

    Sure bet 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '1'; y <= '9';)
+		{
+			putchar(x);
+			putchar(y);
+			if (x < '8' | y < '9')
+			{
+				putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
