187b18795592e4aaa5301d1657a71c28c2aa61ae
Gwinchy <gmunyui254@gmail.com>
2023-07-16 02:33:44 -0700

    Fil=nally Chat GPT came through

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,16 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= 98; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + '1'; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar('0' + a / 10);
 			putchar('0' + a % 10);
 			putchar(' ');
 			putchar('0' + b / 10);
 			putchar('0' + b % 10);
-			if (a != 98 && b != 99)
+			if (a != 98 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
