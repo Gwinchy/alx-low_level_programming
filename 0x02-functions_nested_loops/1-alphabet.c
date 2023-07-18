48870c5418d32d8b4c3c1e28d7fbe459fbb363fa
Gwinchy <gmunyui254@gmail.com>
2023-07-18 03:49:24 -0700

    function that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(char letter);
+void print_alphabet(char letter)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+	}
+	int main(void)
+	{
+		print_alphabet('a');
+		return (0);
+	}
