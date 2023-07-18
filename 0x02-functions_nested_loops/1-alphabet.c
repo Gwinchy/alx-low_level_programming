349f381b6725293ad49cbcfea0d0ebfb4b6acab7
Gwinchy <gmunyui254@gmail.com>
2023-07-18 04:11:34 -0700

    Learnt more about betty and function descriptions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,27 @@
 # include <stdio.h>
 /**
+ * print_alphabet - Prints the lowercase alphabet from 'a' to 'z'
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
+/**
  * main - Starting point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(char letter);
-print_alphabet(char letter)
-	{
-		letter = 'a';
-
-		while (letter <= 'z')
-		{
-			putchar(letter);
-			letter++;
-		}
-		putchar('\n');
-	}
-	int main(void)
-	{
-		print_alphabet('a');
+int main(void)
+{
+		print_alphabet();
 		return (0);
-	}
+}
