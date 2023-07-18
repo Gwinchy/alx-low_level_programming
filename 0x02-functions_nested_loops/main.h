b07c96c9e3c2c996bbe9551d38446d32f48ba8e1
Gwinchy <gmunyui254@gmail.com>
2023-07-18 05:02:28 -0700

    correct

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,16 +12,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
 
 #endif
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
