70658b03e700dd89a4a64ee1d9e2672dfe8bce62
Gwinchy <gmunyui254@gmail.com>
2023-07-25 00:58:54 -0700

    Done writing a function that prints the value of the pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts- function that prints a string to the standard output
+ * @str: string to be printed by the function
+ * Return: void
+ */
+void _puts(char *str)
+{
+	putchar(*str);
+	putchar('\n');
+}
