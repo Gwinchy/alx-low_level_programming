7323e2405f7febdb5c42e29031f4a95a4295ac3d
Gwinchy <gmunyui254@gmail.com>
2023-07-26 12:16:41 -0700

    used strlen to compare the lengths

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,20 +14,19 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0';)
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+
+	if (len1 != len2)
 	{
-		i++;
+		return (-1);
 	}
-	for (i = 0; s2[i] != '\0';)
+	for (i = 0; i < len1; i++)
 	{
-		i++;
-	}
-	if (s1[i] == s2[i])
-	{
-		return (0);
-	}
-		else
+		if (s1[i] != s2[i])
 		{
 			return (-1);
 		}
+	}
+	return (0);
 }
