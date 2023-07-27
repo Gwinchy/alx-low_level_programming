0b75ebbf874ac40e64808e533846a5894c8fe173
Gwinchy <gmunyui254@gmail.com>
2023-07-27 08:04:56 -0700

    corrections on the code made

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "main.h"
 #include <stdio.h>
 #include <string.h>
 /**
@@ -10,7 +9,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -24,6 +24,5 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-
-		return(dest);
+	return(dest);
 }
