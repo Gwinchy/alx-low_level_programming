d40ac204455df5956ae220f2b7cb9da6d7a524c8
Gwinchy <gmunyui254@gmail.com>
2023-07-27 08:12:05 -0700

    3. correction to strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,19 +14,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-
-	if (len1 != len2)
-	{
-		return (-1);
-	}
-	for (i = 0; i < len1; i++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (-1);
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
 	return (0);
 }
