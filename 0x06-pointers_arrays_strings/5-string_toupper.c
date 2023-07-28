82f26e8e06fc8dbd63c041177e4419dfbb2c1912
Gwinchy <gmunyui254@gmail.com>
2023-07-28 03:21:00 -0700

    !! could not configure what the checker needs

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * string_toupper- function that changes to uppercase
- * @str: the strings under operation
+ * @n: the strings under operation
  * Return: Always str (Success)
  */
-char *string_toupper(char *str)
+char *string_toupper(char *)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
-			{
+		{
 			str[i] = str[i] - 32;
 			i++;
-			}
+		}
 	}
 	return (str);
 }
