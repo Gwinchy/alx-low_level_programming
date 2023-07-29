a731e3275abd2d1013bb9867e17a608b1c85193b
Gwinchy <gmunyui254@gmail.com>
2023-07-29 01:30:22 -0700

    removed the other prototypes from the function

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-positive_or_negative();
-printf("Infinite loop incoming :(\n");
-printf("Infinite loop avoided! \\o/\n");
 int largest_number(int a, int b, int c);
 
 
