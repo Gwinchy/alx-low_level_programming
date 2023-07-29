f83e7a5d3a8f72f82fc0180e5e0d263c70650259
Gwinchy <gmunyui254@gmail.com>
2023-07-29 01:00:48 -0700

    0.Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
