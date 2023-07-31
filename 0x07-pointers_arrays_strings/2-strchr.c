5bc7936136104d75d11eacf9efe9847ee6bf6143
Gwinchy <gmunyui254@gmail.com>
2023-07-31 05:32:54 -0700

    2. Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strchr -function that locates
+ * _strchr - function that locates
  * a character in a string
  * @s: a string to search in
  * @c: the character to locate
