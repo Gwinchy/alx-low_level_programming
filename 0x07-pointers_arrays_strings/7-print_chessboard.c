b555fcb7330a72d3fc9e6b1dd98cb1ef16b35704
Gwinchy <gmunyui254@gmail.com>
2023-07-31 06:44:07 -0700

    7. print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard in 2D
+ * @a: 2D array for char
+ *
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			putchar(a[i][j]);
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
