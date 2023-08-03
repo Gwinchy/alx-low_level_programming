23455ad76add9f007a49c1a4d386c41fb7810e3a
Gwinchy <gmunyui254@gmail.com>
2023-08-03 07:30:50 -0700

    improved the code and made the necessary changes to my main.h

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,24 +3,21 @@
 /**
  * is_prime_number - function that returns 1 if number is prime
  * @n: integer to be processed
+ * @i: int to check divisibility
  * Return: 1 if n is prime and 0 if otherwise
  */
-int is_prime_number(int n)
+int is_prime_number(int n, int i)
 {
-	int prime = 1;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	int i;
-
-	for (i = 2; i * i <= n; i++)
+	for (i = 2; i < n; i++)
 	{
 		if (n % i == 0)
 		{
 			return (0);
 		}
 	}
-		return (prime);
+	return (1);
 }
