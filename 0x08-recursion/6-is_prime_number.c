1abc921faeb51360bc53b6a159ce58397ccb9eb4
Gwinchy <gmunyui254@gmail.com>
2023-08-03 11:58:35 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * is_prime_number - function that returns 1 if number is prime
- * @n: integer to be processed
- * @i: int to check divisibility
- * Return: 1 if n is prime and 0 if otherwise
+ * checker - function that checks recursively the
+ * input from is_prime_number
+ *
+ * @n: number to checker
+ * @base: base number to check
+ *
+ * Return: 1 if n is prime, else return 0 otherwise
  */
-int is_prime_number(int n, int i)
+int checker(int n, int base)
 {
-	if (n <= 1)
+	if (base % n == 0 || base < 2)
 	{
 		return (0);
 	}
-	for (i = 2; i < n; i++)
+	else if (n == base - 1)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else if (base > n)
+	{
+		return (checker(n + 1, base));
 	}
 	return (1);
+}
+/**
+ * is_prime_number - function that checks if
+ * a number is prime
+ *
+ * @n: input parameter to check
+ *
+ * Return: 1 if n is prime, else return 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (checker(2, n));
 }
