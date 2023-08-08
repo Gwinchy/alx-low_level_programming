9dc182d0c47122c4c30429ef0885f45d3239de12
Gwinchy <gmunyui254@gmail.com>
2023-08-08 03:40:16 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - starting poing
+ *
+ * @argc: argument count
+ *
+ * @argv: argument value pointer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	for (argc = 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+				printf("%d\n", mul);
+				}
+				return (0);
+				}
