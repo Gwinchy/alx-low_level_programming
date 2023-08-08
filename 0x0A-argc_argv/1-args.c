1df1677d2184011a5a210e4015be7478ad9a0503
Gwinchy <gmunyui254@gmail.com>
2023-08-08 02:05:29 -0700

    First time doing this hope is the last time also

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * @argc: argument counter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
