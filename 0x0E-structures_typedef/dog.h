ab54773cfa397edecc9d15599eb2074a4fb9ec1b
Gwinchy <gmunyui254@gmail.com>
2023-08-14 22:21:44 -0700

    Correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ *
+ * @name: name of the dog
+ * @age : age of the dog
+ * @owner: dog's owner
+ *
+ * Description: Defines metadata about
+ * the dog and type of dog's struct
+ */
 struct dog {
-	char name[];
+	char *name[];
 	float age;
-	char owner;
+	char *owner;
 };
+typdef struct dog dog_t;
+void int_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
