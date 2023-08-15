711e6903374b5a8dd8f17fe0b388809d26a0cc90
Gwinchy <gmunyui254@gmail.com>
2023-08-15 00:38:15 -0700

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,8 +11,17 @@
  * the dog and type of dog's struct
  */
 struct dog {
-	char *name[];
+	char *name;
 	float age;
 	char *owner;
 };
+
+void int_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
