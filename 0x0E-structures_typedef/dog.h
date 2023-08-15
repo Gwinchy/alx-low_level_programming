376a8e3a0b39524a8501ca1928147b63ecdd78a1
Gwinchy <gmunyui254@gmail.com>
2023-08-15 00:03:05 -0700

    correct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,4 @@ struct dog {
 	float age;
 	char *owner;
 };
-typdef struct dog dog_t;
-void int_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
