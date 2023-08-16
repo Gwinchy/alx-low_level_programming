91cc096e4af7a0bfe64b7bd8709e1eeb79a0a391
Gwinchy <gmunyui254@gmail.com>
2023-08-16 04:54:15 -0700

    Correction_01

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,21 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "function_pointers.h"
+
 /**
- * print_name - function that prints a name
- *
- * @name: name to be printed
- *
- * @f: pointer to the string name
- *
- * Return: Always 0 (Success)
- */
+* print_name - function to print a name
+*
+* @name: string input
+* @f: pointer to a function casted to char
+*
+* Return: nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	while (*name != '\0')
-	{
-		putchar(*name);
-		name++;
-	}
-	putchar('\n');
-	f(name);
+if (name == NULL || f == NULL)
+return;
+(*f)(name);
 }
