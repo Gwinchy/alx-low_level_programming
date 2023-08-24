bc087ae81b508c44bd41380dd3aeb7cf0ae73521
Gwinchy <gmunyui254@gmail.com>
2023-08-24 06:15:24 -0700

    4. Free list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,10 +22,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->len = strlen(str);
 	new_node->next = NULL;
+	if (*head == NULL)
 	{
-		current = current->next;
+		*head = new_node;
+		return (new_node);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+	current = current->next;
 	}
 	current->next = new_node;
 	return (new_node);
