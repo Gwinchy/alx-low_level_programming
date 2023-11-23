54d2644b36d79642c2f7c44c290f35a813340bbd
root <gmunyui254@gmail.com>
2023-11-23 12:27:15 -0800

    Changed the logical operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,14 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
-	if (*b != '1' || *b != '0')
+	if (*b != '1' && *b != '0')
 	{
 		return (0);
 	}
 	while (*b != '\0')
 	{
 		result = (result << 1) + (*b - '0');
+		b++;
 	}
 	return (result);
 }
--- 0x14-bit_manipulation/test/0-binary_to_uint.c
+++ 0x14-bit_manipulation/test/0-binary_to_uint.c
@@ -8,9 +8,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
+	if (*b == '\0')
+	{
+		return (0);
+	}
+	if (*b != '1' && *b != '0')
+	{
+		return (0);
+	}
 	while (*b != '\0')
 	{
 		result = (result << 1) + (*b - '0');
+		b++;
 	}
 	return (result);
 }
