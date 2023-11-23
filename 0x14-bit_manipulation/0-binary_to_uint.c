ecf0cda474f94ac88678edba6e80dedf9cc67088
root <gmunyui254@gmail.com>
2023-11-23 11:02:58 -0800

    Added the negative conditions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,6 +8,14 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
+	if (*b == '\0')
+	{
+		return (0);
+	}
+	if (*b != '1' || *b != '0')
+	{
+		return (0);
+	}
 	while (*b != '\0')
 	{
 		result = (result << 1) + (*b - '0');
