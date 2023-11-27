fd40d52b01cf5dc7911830e8b445e26d395e4fdd
root <gmunyui254@gmail.com>
2023-11-27 00:35:35 -0800

    Replaced the system calls with the required ones

--- 0x15-file_io/test/0-read_textfile.c
+++ 0x15-file_io/test/0-read_textfile.c
@@ -16,6 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+
 	buffer = malloc(letters + 1);
 	if (buffer == NULL)
 	{
--- 0x15-file_io/test/main.h
+++ 0x15-file_io/test/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #ifndef MAIN_H
 #define MAIN_H
 ssize_t read_textfile(const char *filename, size_t letters);
