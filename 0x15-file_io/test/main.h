b0f5f188213c645fb309e82d3875db70cf32bd0c
root <gmunyui254@gmail.com>
2023-11-27 03:31:16 -0800

    1. Under the snow

--- 0x15-file_io/test/main.h
+++ 0x15-file_io/test/main.h
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
 #ifndef MAIN_H
 #define MAIN_H
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
