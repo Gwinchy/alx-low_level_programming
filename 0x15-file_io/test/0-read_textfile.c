2d31f8cc950117d92ab66048b53a789b2e72c1ed
root <gmunyui254@gmail.com>
2023-11-26 23:53:07 -0800

    the file reads a deficiency of 10 chars

--- 0x15-file_io/test/0-read_textfile.c
+++ 0x15-file_io/test/0-read_textfile.c
@@ -1,53 +1,40 @@
 #include "main.h"
 /**
-* read_textfile - function that reads a text file
-* and prints it to the POSIX stdout
-*
-* @filename: name of file
-* @letters: number of characters
-*
-* Return: 0 on success
-**/
+ * read_textfile - reads a textfile and outputs to POSIX stdout
+ * @filename: name of the file
+ * @letters: data in the file
+ * Return: 0 or actual size of readeable input
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int text;
-char *buffer;
-ssize_t total, status;
+	FILE *file = fopen(filename, "r");
+	char *buffer;
+	ssize_t bytesRead, bytesWritten;
 
-if (!filename)
-return (0);
-status = open(filename, O_RDONLY);
-if (status == -1)
-{
-close(status);
-return (0);
-}
-
-buffer = malloc(sizeof(char) * letters);
-if (!buffer)
-{
-close(status);
-return (0);
-}
-
-total = read(status, buffer, letters);
-
-if (total == -1)
-{
-close(status);
-free(buffer);
-return (0);
-}
-
-text = write(STDOUT_FILENO, buffer, total);
-
-if (text == -1)
-{
-close(status);
-free(buffer);
-return (0);
-}
-close(status);
-return (total);
+	if (file == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return(0);
+	}
+	bytesRead = fread(buffer, sizeof(char), letters, file);
+	if (bytesRead < 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	buffer[bytesRead] = '\0';
+	bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
+	if (bytesWritten < 0)
+	{
+		fclose(file);
+		free(buffer);
+	}
+	return (bytesWritten);
 }
