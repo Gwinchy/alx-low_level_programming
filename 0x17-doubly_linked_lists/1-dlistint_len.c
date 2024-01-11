f2de879ff801ce55a74126a9f766ba1cc7956537
Gwinchy <gmunyui254@gmail.com>
2024-01-11 12:34:21 -0800

    removed the unnecessary print function and now the two have been freed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 /**
- * print_dlistint - function that prints linked lists
+ * dlistint_len - function that prints linked lists
  * @h: pointer to the head of the struct
  * Return: linked lists , count
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
 
 	while (current != NULL)
 	{
-		printf("%d\n", current->n);
 		current = current->next;
 		count++;
 	}
